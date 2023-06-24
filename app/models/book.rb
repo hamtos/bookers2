001178556f62eb118d6f7cf62de2c055ce66c815
hamtos <135288715+hamtos@users.noreply.github.com>
2023-06-24 06:06:54 +0000

    [add]all_views

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,2 +1,15 @@
 class Book < ApplicationRecord
+
+ has_one_attached :book_image
+
+ belongs_to :user
+
+ def get_book_image(width, height)
+   unless book_image.attached?
+     file_path = Rails.root.join('app/assets/images/142_refileDownloadImage.png')
+     book_image.attach(io: File.open(file_path), filename: 'default-image.jpg', content_type: 'image/jpeg')
+   end
+     book_image.variant(resize_to_limit: [width, height]).processed
+ end
+
 end
